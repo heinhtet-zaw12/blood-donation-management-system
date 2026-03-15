a685b599379c28d4077d219d03a1e037bb6a3dbc
SoeYuLay <zaya532002@gmail.com>
2026-03-15 22:39:28 +0630

    request

--- lib/core/widgets/label_text_widget.dart
+++ lib/core/widgets/label_text_widget.dart
@@ -1,10 +1,10 @@
 import 'package:blood_donation_management_system/core/theme/extension/bdms_text_theme.dart';
 import 'package:flutter/material.dart';
 
-Widget buildLabel(String label , Color color ,BDMSTextTheme textTheme, {Alignment alignment=Alignment.centerLeft} ) {
+Widget buildLabel(String label , Color color ,BDMSTextTheme textTheme,  {Alignment alignment=Alignment.centerLeft}) {
   return Align(
     alignment:  alignment,
-    child: Text(label, style: textTheme.tabText.copyWith(
+    child: Text(label,textAlign: TextAlign.center, style: textTheme.tabText.copyWith(
       color: color
     )),
   );
